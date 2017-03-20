7e813ce9d5f2c84204fe7034ed8cb2bb6fc7bcab
ZssMac <zhangshangshang@mallog.cnzhangshangshang@mallog.cnzhangshangshang@mallog.cnzhangshangshang@mallog.cnzhangshangshang@mallog.cn>
2017-03-20 21:02:04 +0800

    添加统计

---
+++ public/javascripts/tongji.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zss on 2017/3/20.
+ */
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?8ee96d7f7ca0752fd02fc13d341c9104";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
